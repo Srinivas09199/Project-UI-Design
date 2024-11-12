91371c632a1a2178e55e15553ff20be7f08f28d8
Srinivas09199 <85751494+Srinivas09199@users.noreply.github.com>
2024-11-12 17:54:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// jobcard.js
+document.querySelectorAll('.view-job').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Job details will be shown here.');
+    });
+});
